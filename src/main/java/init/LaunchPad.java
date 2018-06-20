5184fa8bfdae1e8dd974d3e4e6ee59a8ae1da6dd
= <dvsocean@icloud.com>
2018-06-19 20:21:05 -0700

    added spring maven plugin to run

--- src/main/java/init/LaunchPad.java
+++ src/main/java/init/LaunchPad.java
@@ -2,16 +2,24 @@ package init;
 
 import objects.Ocean;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class LaunchPad {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = new FileSystemXmlApplicationContext("bean.xml");
+//        ApplicationContext ctx = new FileSystemXmlApplicationContext("bean.xml");
+//        Ocean o = (Ocean) ctx.getBean("ocean");
+//
+//        o.organization();
+//
+//        ((FileSystemXmlApplicationContext) ctx).close();
+
+        //Must put bean on classpath aka resources folder
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("bean-cp.xml");
         Ocean o = (Ocean) ctx.getBean("ocean");
 
         o.organization();
 
-        ((FileSystemXmlApplicationContext) ctx).close();
+        ((ClassPathXmlApplicationContext) ctx).close();
     }
 }
